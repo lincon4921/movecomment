c20e2e5ff2301104e163bf8f0f152299c7adcd24
TÉCNICO EM INFORMÁTICA 2019.1 <36121742019.1@senacrjedu.local>
2021-02-03 11:51:11 -0300

    criação do perfil

---
+++ src/app/page/perfil/perfil.page.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+@Component({
+  selector: 'app-perfil',
+  templateUrl: './perfil.page.html',
+  styleUrls: ['./perfil.page.scss'],
+})
+export class PerfilPage implements OnInit {
+
+  ngOnInit(): void {}
+
+  constructor(private menu: MenuController) { }
+
+  openFirst() {
+    this.menu.enable(true, 'first');
+    this.menu.open('first');
+  }
+
+  openEnd() {
+    this.menu.open('end');
+  }
+
+  openCustom() {
+    this.menu.enable(true, 'custom');
+    this.menu.open('custom');
+  }
+
+}
